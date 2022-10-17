78128facdeae719c773caf833361b694792c5a86
tianjingli <569302975@qq.com>
2022-10-17 10:36:25 +0800

    rename project.

--- src/main/java/com/tjl/RunApplication.java
+++ src/main/java/com/tjl/RunApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EncryptFileTransApplication {
+public class RunApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EncryptFileTransApplication.class, args);
+        SpringApplication.run(RunApplication.class, args);
     }
 
 }
